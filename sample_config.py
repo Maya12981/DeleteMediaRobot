c29fd3cb7054f6e26b5b1f4cf145259e899e0ba5
Maya12981 <158100710+Maya12981@users.noreply.github.com>
2024-06-07 13:19:13 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -22,19 +22,19 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 class Config(object):
 
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7148114178:AAGpKYE4hsBg7INn0LqPryseKvSrreP_ZSM")
 
     # Get from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", ""))
+    APP_ID = int(os.environ.get("APP_ID", "22894706"))
 
     # Get from my.telegram.org
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "13c8f765d49c935d2ffd9152f8430f7e")
 
     # Generate a user session string
-    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
+    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BQC6kfsABhUpjLg4BUPOk-ot7uV_MX25n370c6mwHvUFp1GMAaJEwYHg_UEl4jTqGtGg0Zm_ijo4UzB8VMUVHCVSEW-6zrPXMu5sWE3tZpamh7isq0pKwdw8IMHpyGCjy35DimhKibNvkUt1Zy9bfABJPIuUhuyTqcP9aGmt3HAyJF3dj7UgdyLwjDcURuZ636UicPqKpYFj1wR2fqvIJwCsVcEkh0DQmYsQrhN_kthgde6OWU_4wponGLG2-6cftFwvFX_HziYSGhCK4YisiJcY6lOZcQ4jPxRD9ENsMHrEtst_lK7NkXCGdJ_I4ZHxcbvKebOMenENtJOBvaI4c-fG7GGCCwAAAAGnUoSaAA")
 
     # Authorized users to perform delete messages in group
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "6797820880").split())
 
 
 def LOGGER(name: str) -> logging.Logger:
